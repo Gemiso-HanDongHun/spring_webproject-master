07ba610f2abdcb1bfd2431989668412e23a9addd
LUYCVN <dh5777895@naver.com>
2022-11-01 14:16:46 +0900

    add

--- src/main/java/com/project/web_prj/config/DataBaseConfig.java
+++ src/main/java/com/project/web_prj/config/DataBaseConfig.java
@@ -4,35 +4,29 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
-// DB 관련 설정 클래스
+// DB 접속 관련 설정 (접속, 커넥션풀...)
 @Configuration
-@ComponentScan(basePackages = "com.project.web_prj")
 @PropertySource("classpath:db_info.properties")
 public class DataBaseConfig {
 
-    @Value("${aws.rds_user_name}")
+    @Value("${local.db.username}")
     private String username;
 
-    @Value("${aws.rds_password}")
+    @Value("${local.db.password}")
     private String password;
 
-    @Value("${aws.rds_url}")
+    @Value("${local.db.url}")
     private String url;
 
-
-
-    // DB접속 정보 설정 (DataSource설정)
     @Bean
     public DataSource dataSource() {
 
         HikariConfig config = new HikariConfig();
-
         config.setUsername(username);
         config.setPassword(password);
         config.setJdbcUrl(url);
